726c1b5125a64dbdda40c436a57db24c02d8d6e6
Mike <parabuzzle@gmail.com>
2010-03-16 16:14:48 -0700

    Changed the sidebar to be ajaxy, moved javascript to foot of page and made some visiual fixes

--- application/app/helpers/site_helper.rb
+++ application/app/helpers/site_helper.rb
@@ -3,17 +3,17 @@ module SiteHelper
     blogrss = "http://spotadouche.wordpress.com/feed/"
     cachedir = RAILS_ROOT + '/cache'
     FileUtils.mkdir_p(cachedir) unless File.directory?(cachedir)
-    file = "#{cachedir}/blog.yml"
+    file = "#{cachedir}/blog.db"
     
-    if File.exist?(file) and File.mtime(file)>Time.now - 0 #cache for 5 minutes
+    if File.exist?(file) and File.mtime(file)>Time.now - SITE_PROPS['admin']['blogcache']
       logger.debug "blog rss cache file is valid... using it"
-      content = YAML::load(File.open(file))
+      content = File.new(file)
     else
       logger.debug "blog rss cache file is old... refreshing"
        # raw content of rss feed will be loaded here
       open(blogrss) do |s| content = s.read end
       f = File.new(file,"w")
-      f.write(YAML::dump(content))
+      f.write(content)
     end
     return RSS::Parser.parse(content, false)
   end  
--- application/public/javascripts/application.js
+++ application/public/javascripts/application.js
@@ -1,6 +1,7 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 jQuery.facebox.settings.addparam = "facebox";
-jQuery.ajaxSetup({'beforeSend': function(xhr) {xhr.setRequestHeader("Accept", "text/javascript")}})
-jQuery(document).ready(function($) {$('a[rel*=facebox]').facebox() })
-
+jQuery.ajaxSetup({'beforeSend': function(xhr) {xhr.setRequestHeader("Accept", "text/javascript")}});
+jQuery(document).ready(function($) {$('a[rel*=facebox]').facebox();
+$("#rss").load("/site/rss .content");
+});
