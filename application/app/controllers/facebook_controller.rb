6177fdba9a1d5c1c09a0ee85a4b6d5dc62f711cc
Mike <parabuzzle@gmail.com>
2010-03-16 10:18:48 +0000

    Wholly shit the facebook is working now

--- application/app/controllers/facebook_controller.rb
+++ application/app/controllers/facebook_controller.rb
@@ -1,6 +1,7 @@
 class FacebookController < ApplicationController
   def index
     @title = "Spot a Douche - Facebook"
+    @photo = Photo.find(:first, :order => 'RAND()')
     render :template => false
   end
 end
