f22aad0d5fab96b34dd0b709b4942a33558a386d
Mike <parabuzzle@gmail.com>
2010-03-17 01:02:59 -0700

    General clean up preparing for release and facebook app tweaking

--- application/app/controllers/facebook_controller.rb
+++ application/app/controllers/facebook_controller.rb
@@ -1,17 +1,16 @@
 class FacebookController < ApplicationController
   def index
     @title = "Spot a Douche - Facebook"
-    #@photo = Photo.find(:first, :order => 'RAND()', :conditions => "status >= 5")
-    @photo = Photo.last
+    @photo = Photo.find(:first, :order => 'RAND()', :conditions => "status >= 5")
+    #@photo = Photo.last
     if params[:vote] != nil
       if params[:vote] == "up"
         @photo.vote_up!
       elsif params[:vote] == "down"
         @photo.vote_down!
       end
-      params[:last] = @photo.id
+      @photo = Photo.find(params[:last]) unless params[:last].nil?
     end
-    @last = Photo.find(params[:last]) unless params[:last].nil? 
     render :template => false
   end
 end
